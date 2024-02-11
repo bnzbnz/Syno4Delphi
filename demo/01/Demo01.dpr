fe085768be566509aa7449136293bbef309f32ac
Laurent Meyer <lmeyer@ea4d.com>
2024-02-11 09:43:09 +0100

    Working Demo 01

--- demo/01/Demo01.dpr
+++ demo/01/Demo01.dpr
@@ -5,7 +5,12 @@ uses
   uDemo01 in 'uDemo01.pas' {FrmDemo01},
   uNetUtils in '..\..\source\uNetUtils.pas',
   uSynoDiscovery in '..\..\source\uSynoDiscovery.pas',
-  uSyno.Types in '..\..\source\uSyno.Types.pas';
+  uSyno.Types in '..\..\source\uSyno.Types.pas',
+  JsonDataObjects in '..\..\source\JSON\JsonDataObjects.pas',
+  uJsonX in '..\..\source\JSON\uJsonX.pas',
+  uJsonX.RTTI in '..\..\source\JSON\uJsonX.RTTI.pas',
+  uJsonX.Types in '..\..\source\JSON\uJsonX.Types.pas',
+  uJsonX.Utils in '..\..\source\JSON\uJsonX.Utils.pas';
 
 {$R *.res}
 
